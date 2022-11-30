0526a12a73c975aa462f8905fa37d676e661330d
Tellerium <77554038+Tellerium@users.noreply.github.com>
2022-11-30 17:32:33 -0600

    Delete PlayerNode.py
    Deprecated via Consolidation

--- python/PlayerNode.py
+++
@@ -1,7 +0,0 @@
-import CharNode
-
-class PlayerNode(CharNode):
-    def __init(self, priority, maxHP, name, head = None, tail = None):
-        super.init(priority, maxHP, name, head, tail)
-        self.IS_AI = False
-        self.IS_ALLY = True
