43b3e540794d42e556d1470c6394c8f222330591
Tellerium <77554038+Tellerium@users.noreply.github.com>
2022-12-01 12:20:31 -0600

    Updated main.py
    Added support for toggling full screen. Full screen scales to display, whereas Window is a 1280 x 720 default.
    Now renders program icon and caption.

--- python/main.py
+++ python/main.py
@@ -2,11 +2,10 @@ import globals, pygame, sys
 from classes import *
 
 pygame.init()
-
-SCREEN = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Project Idetidem")
-
-#BG = pygame.image.load('./assets/titleBG.png')
+WINDOW = pygame.display.set_mode((1280, 720), pygame.SCALED)
+ICON = pygame.image.load("./assets/images/test.ico")
+pygame.display.set_caption("Project Identidem")
+pygame.display.set_icon(ICON)
 
 def get_font(size, fontKey):
     fontDict = {
@@ -18,23 +17,27 @@ def get_font(size, fontKey):
 
 def title():
     pygame.display.set_caption("Project Identidem")
-    SCREEN.fill("black")
+    WINDOW.fill("black")
 
     while True:
-        #SCREEN.blit(BG, (0, 0))
-
         MENU_TEXT = get_font(64, 'title').render("Identidem", True, 'Purple')
-        MENU_TEXT_SHADOW = get_font(65, 'title').render("Identidem", True, 'White')
-        SCREEN.blit(MENU_TEXT_SHADOW, (478, 72))
-        SCREEN.blit(MENU_TEXT, (480, 72))
+        WINDOW.blit(MENU_TEXT, (480, 72))
 
-        INSTRUCTION_TEXT = get_font(32, 'text').render("UP/DOWN To Select", True, "White")
-        SCREEN.blit(INSTRUCTION_TEXT, (16, 672))
+        INSTRUCTION_TEXT = get_font(32, 'text').render("UP/DOWN To Select ", True, "White")
+        WINDOW.blit(INSTRUCTION_TEXT, (16, 672))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == pygame.K_f:
+                    pygame.display.toggle_fullscreen()
+
         pygame.display.update()
 
 if __name__ == '__main__':
