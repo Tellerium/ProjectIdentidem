4a13f6b749aa28ec6dc2160a22bb6650f2a22d45
Tellerium <77554038+Tellerium@users.noreply.github.com>
2022-12-09 08:54:59 -0600

    Refactored Framing
    Added support for independent frames.
    Added navigation layout.

--- python/globals.py
+++ python/globals.py
@@ -1,7 +1,16 @@
 import xml.etree.ElementTree as ET
+import math, time
 
 def init():
     global charTree
-    charTree = ET.parse('./xml/characters.xml')
     global charRoot
+    global seed
+    global frame
+    
+    charTree = ET.parse('./xml/characters.xml')
     charRoot = charTree.getroot()
+    seed = 0
+    frame = 'title'
+
+def newSeed():
+    return (int(math.sqrt((math.pi * math.cos(time.time()) * time.time()) ** 2) // 2))
--- python/main.py
+++ python/main.py
@@ -16,11 +16,17 @@ def get_font(size, fontKey):
 
     return pygame.font.Font(str("./assets/fonts/"+ fontDict[fontKey] + ".ttf"), size)
 
+def game():
+    while True:
+        options()
+        crossroads()
+        title()
+
 def title():
     #Initialize Window
     pygame.display.set_caption("Project Identidem")
     WINDOW.fill("black")
-    
+    onTitle = True
     #Initialize Title & Instructions
     #Set Text
     titleText = get_font(64, 'title').render("Identidem", True, 'Purple')
@@ -79,7 +85,7 @@ def title():
     WINDOW.blit(selectedText, selectedRect)
 
     #Perform Title Screen Processing
-    while True:
+    while globals.frame == 'title':
     #Update Selected Text
         if indexChanged != -1:
             selectedText = get_font(32, 'text').render(textDict.get(str(indexChanged)), True, "Gray")
@@ -109,10 +115,69 @@ def title():
                     #Update Index
                     indexChanged = textIndex
                     textIndex = (textIndex - 1) % 4
+                if event.key == (pygame.K_BACKSPACE):
+                    if textIndex == 0:
+                        new_Game()
+                    elif textIndex == 1:
+                        load_Game()
+                    elif textIndex == 2:
+                        globals.frame = 'options'
+                    elif textIndex == 3:
+                        pygame.quit()
+                        sys.exit()
 
+        pygame.display.update()
+
+def new_Game():
+    globals.seed = globals.newSeed()
+    globals.frame = 'cross'
+
+def load_Game():
+    globals.frame = 'cross'
+
+def options():
+    pygame.display.set_caption('Options')
+    WINDOW.fill("green")
+    
+    while globals.frame == 'options':
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_f:
+                        pygame.display.toggle_fullscreen()
+                    if event.key == (pygame.K_z):
+                        globals.frame = 'title'
+        
+        pygame.display.update()
+
+def crossroads():
+    pygame.display.set_caption(str(globals.seed))
+    WINDOW.fill("pink")
+
+    while globals.frame == 'cross':
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                    if event.key == pygame.K_f:
+                        pygame.display.toggle_fullscreen()
+                    if event.key == (pygame.K_z):
+                        globals.frame = 'title'
+    
         pygame.display.update()
 
 if __name__ == '__main__':
     globals.init()
-    title()
+    game()
     
